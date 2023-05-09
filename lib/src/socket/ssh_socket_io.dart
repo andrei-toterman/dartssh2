a857d2734011bdbbdde47090312a61e9401a8905
Andrei Toterman <andrei.toterman@gmail.com>
2023-05-09 17:22:10 +0200

    use RawSocket instead of Socket for more customization

--- lib/src/socket/ssh_socket_io.dart
+++ lib/src/socket/ssh_socket_io.dart
@@ -9,20 +9,59 @@ Future<SSHSocket> connectNativeSocket(
   int port, {
   Duration? timeout,
 }) async {
-  final socket = await Socket.connect(host, port, timeout: timeout);
+  final socket = await RawSocket.connect(host, port, timeout: timeout);
   return _SSHNativeSocket._(socket);
 }
 
 class _SSHNativeSocket implements SSHSocket {
-  final Socket _socket;
+  final RawSocket _socket;
+  final Completer<void> _doneCompleter = Completer();
+  final StreamController<Uint8List> _remoteController = StreamController();
+  final StreamController<Uint8List> _localController = StreamController(
+    sync: true,
+  );
+  List<int> _writeBuffer = Uint8List(0);
 
-  _SSHNativeSocket._(this._socket);
+  _SSHNativeSocket._(this._socket) {
+    _socket.readEventsEnabled = true;
+    _socket.writeEventsEnabled = false;
+    _socket.listen((event) {
+      if (event == RawSocketEvent.closed ||
+          event == RawSocketEvent.readClosed) {
+        if (!_doneCompleter.isCompleted) _doneCompleter.complete();
+        // _removeSocket(_socket);
+      }
+
+      if (event == RawSocketEvent.write) {
+        if (_writeBuffer.isEmpty) return;
+        final written = _socket.write(_writeBuffer);
+        _writeBuffer = _writeBuffer.sublist(written);
+        if (_writeBuffer.length != 0) _socket.writeEventsEnabled = true;
+      }
+
+      if (event == RawSocketEvent.read) {
+        _socket.readEventsEnabled = false;
+        final read = _socket.read(1024 * 4);
+        if (read != null) _remoteController.add(read);
+        Timer(const Duration(milliseconds: 1), () {
+          _socket.readEventsEnabled = true;
+        });
+      }
+    });
+
+    _localController.stream.listen((data) {
+      _writeBuffer = _writeBuffer + data;
+      _socket.writeEventsEnabled = true;
+    });
+
+    // _addSocket(_socket, _remoteController.sink);
+  }
 
   @override
-  Stream<Uint8List> get stream => _socket;
+  Stream<Uint8List> get stream => _remoteController.stream;
 
   @override
-  StreamSink<List<int>> get sink => _socket;
+  StreamSink<List<int>> get sink => _localController;
 
   @override
   Future<void> close() async {
@@ -30,11 +69,11 @@ class _SSHNativeSocket implements SSHSocket {
   }
 
   @override
-  Future<void> get done => _socket.done;
+  Future<void> get done => _doneCompleter.future;
 
   @override
   void destroy() {
-    _socket.destroy();
+    _socket.close();
   }
 
   @override
@@ -42,4 +81,29 @@ class _SSHNativeSocket implements SSHSocket {
     final address = '${_socket.remoteAddress.host}:${_socket.remotePort}';
     return '_SSHNativeSocket($address)';
   }
+
+  // static Timer? _readTimer;
+  // static Map<RawSocket, Sink<Uint8List>> _sockets = {};
+
+  // static void _addSocket(RawSocket socket, Sink<Uint8List> sink) {
+  //   _sockets[socket] = sink;
+  //   _readTimer ??= Timer.periodic(const Duration(milliseconds: 1), (_) {
+  //     for (final entry in _sockets.entries) {
+  //       final socket = entry.key;
+  //       final sink = entry.value;
+  //       if (socket.available() > 0) {
+  //         final read = socket.read(1024 * 4);
+  //         if (read != null) sink.add(read);
+  //       }
+  //     }
+  //   });
+  // }
+
+  // static void _removeSocket(RawSocket socket) {
+  //   _sockets.remove(socket);
+  //   if (_sockets.isEmpty) {
+  //     _readTimer?.cancel();
+  //     _readTimer = null;
+  //   }
+  // }
 }
